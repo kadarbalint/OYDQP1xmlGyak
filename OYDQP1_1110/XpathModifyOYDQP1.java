d72ef97ae4c41326379a5167ffb8a37728ee2f52
kadarbalint <65283137+kadarbalint@users.noreply.github.com>
2020-11-24 12:20:27 +0100

    Add files via upload

---
+++ OYDQP1_1110/XpathModifyOYDQP1.java
@@ -0,0 +1,43 @@
+package OYDQP1_1110;
+
+import java.io.*;
+import javax.xml.parsers.*;
+import javax.xml.xpath.*;
+import org.w3c.dom.*;
+import org.xml.sax.*;
+
+public class XpathModifyH0SRE6 {
+    public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException, XPathExpressionException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File("OYDQP1_1110\\studentOYDQP1.xml"));
+        XPathFactory xpathFactory = XPathFactory.newInstance();
+        XPath xpath = xpathFactory.newXPath();
+
+        String expression = "/class/student";
+        NodeList nodes = (NodeList) xpath.compile(expression).evaluate(doc, XPathConstants.NODESET);
+
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node node = nodes.item(i);
+            Element element = (Element) node;
+
+            String rollno = element.getAttribute("rollno");
+            System.out.println("\ncurrent element: " + node.getNodeName());
+            System.out.println("student roll no: " + rollno);
+
+            NodeList childNodes = element.getChildNodes();
+
+            for (int j = 0; j < childNodes.getLength(); j++) {
+                Node childNode = childNodes.item(j);
+
+                if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+                    if (rollno.equals("393") && childNode.getNodeName() == "lastname") {
+                        childNode.setTextContent("Zöld");
+                    }
+
+                    System.out.println(childNode.getNodeName() + ": " + childNode.getTextContent());
+                }
+            }
+        }
+    }
+}
---
+++ OYDQP1_1110/XpathQueryOYDQP1.java
@@ -0,0 +1,38 @@
+package OYDQP1_1110;
+
+import java.io.*;
+import javax.xml.parsers.*;
+import javax.xml.xpath.*;
+import org.w3c.dom.*;
+import org.xml.sax.*;
+
+public class XpathQueryH0SRE6 {
+    public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException, XPathExpressionException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File("OYDQP1_1110\\studentOYDQP1.xml"));
+        XPathFactory xpathFactory = XPathFactory.newInstance();
+        XPath xpath = xpathFactory.newXPath();
+
+        String expression = "/class/student[@rollno='593']";
+        NodeList nodes = (NodeList) xpath.compile(expression).evaluate(doc, XPathConstants.NODESET);
+
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node node = nodes.item(i);
+            Element element = (Element) node;
+
+            System.out.println("\ncurrent element: " + node.getNodeName());
+            System.out.println("student roll no: " + element.getAttribute("rollno"));
+
+            NodeList childNodes = element.getChildNodes();
+
+            for (int j = 0; j < childNodes.getLength(); j++) {
+                Node childNode = childNodes.item(j);
+
+                if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+                    System.out.println(childNode.getNodeName() + ": " + childNode.getTextContent());
+                }
+            }
+        }
+    }
+}
